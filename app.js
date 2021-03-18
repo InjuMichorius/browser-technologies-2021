81847c0daf2ce310e7d08b7499bb3942099fadc8
ÏnjuMichorius <Kato1234!>
2021-03-18 13:42:15 +0100

    Wrote HTML and started on db implementation

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+require('dotenv').config()
+
+// const url = "mongodb+srv://" + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + "KEY"
+console.log(process.env.TEST)
+
+
 //Identifying default path
 app.use(express.static(__dirname + '/public/'));
 
