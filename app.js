7959ba14c9c0548d3bd58471062770a38dff6054
ÏnjuMichorius <Kato1234!>
2021-03-25 13:06:17 +0100

    testing

--- app.js
+++ app.js
@@ -11,36 +11,59 @@ const { MongoClient } = require('mongodb')
 //Config our .env file
 require('dotenv').config()
 
+//Defining db
+let db
+
 //Connect to database
-const url = 'mongodb+srv://' + process.env.NAME + ':' + process.env.DB_PASSWORD + '@cluster0.zlqtj.mongodb.net/' + process.env.DB_NAME + '?retryWrites=true&w=majority'
+const url = 'mongodb+srv://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@cluster0.zlqtj.mongodb.net/enquetes?retryWrites=true&w=majority'
 
 mongo.MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
   if(err) {
     throw err
   }
   db = client.db(process.env.DB_NAME)
+
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
 })
 
-//Insert data to the database
-app.post('/send-enquete', sendEnquete)
-
-function sendEnquete(req, res, next) {
-  db.collection('enquete').insertOne({
-    studentName: req.body.studentName,
-    studentNumber: req.body.studentNumber
-  }, done)
-  function done(err) {
-    if (err) {
-      next(err)
-    } else {
-      res.redirect('/succes')
-    }
-  }
-}
-
-router.use(bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({
   extended: true,
 }));
+
+//Insert data to the database
+// app.post('/send-enquete', register)
+
+// function register(req, res, next) {
+//   db.collection('enquete').insertOne({
+//     studentName: req.body.studentName,
+//     studentNumber: req.body.studentNumber
+//   }, done)
+//   function done(err) {
+//     if (err) {
+//       next(err)
+//     } else {
+//       res.redirect('/succes')
+//     }
+//   }
+// }
+
+function userInput(req, res, next) {
+  req.body.studentName
+  console.log(studentName.value())
+}
+userInput()
+
+// app.post('/send-enquete', user)
+
+// function user(req, res, next) {
+//     db.collection('enquete').findOne(studentName)
+//     console.log(studentName)
+
+// }
+
 
 //Identifying default path
 app.use(express.static(__dirname + '/public/'));
@@ -52,8 +75,3 @@ app.set('view engine', 'ejs')
 //Routes
 app.get('/', enquete)
 app.get('/succes', succes)
-
-//Server check
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
