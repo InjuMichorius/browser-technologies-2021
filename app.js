bf7512375a4ddb4638c047d8e636ae9991d14468
ÏnjuMichorius <Kato1234!>
2021-03-26 10:00:13 +0100

    Added useNewUrlParser and useUnifiedTopogoly

--- app.js
+++ app.js
@@ -1,77 +1,46 @@
+const { v4: uuidv4 } = require('uuid');
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
-const router = express.Router()
-const port = 3000
 const account = require('./public/source/routes/account.js')
 const q1WAFS = require('./public/source/routes/q1WAFS.js')
 const q2WAFS = require('./public/source/routes/q2WAFS.js')
 const q3WAFS = require('./public/source/routes/q3WAFS.js')
-const mongo = require('mongodb')
 const mongoose = require('mongoose')
-const { MongoClient } = require('mongodb')
-
 
 //Config our .env file
 require('dotenv').config()
 
-//Defining db
-let db
-
 //Connect to database
 const url = 'mongodb+srv://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@cluster0.zlqtj.mongodb.net/enquetes?retryWrites=true&w=majority'
 
-mongo.MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
-  if(err) {
-    throw err
-  }
-  db = client.db(process.env.DB_NAME)
+// verhuizen naar een mongoose service
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useUnifiedTopology', true)
+mongoose.connect(url);
+const Schema = mongoose.Schema;
 
+const StudentDataSchema = new Schema({
+  uuid: { type: String, required: true },
+  studentName: { type: String, required: true },
+  studentNumber: { type: String, required: true },
+}, { collection: 'Student'});
 
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
-})
+const WAFSDataSchema = new Schema({
+  uuid: { type: String, required: true },
+  startDate: { type: String, required: true },
+  endDate: { type: String, required: true },
+  lessonMarterial: { type: String, required: true},
+  explanation: { type: String, required: true},
+  ownInsight: { type: String, required: true}
+}, { collection: 'WAFS'});
 
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
+const StudentPersistence = mongoose.model('Student', StudentDataSchema);
+const WAFSPersistence = mongoose.model('WAFS', WAFSDataSchema);
+//
 
-
-
-//Insert data to the database
-app.post('/send-account', user)
-
-function user(req, res, next) {
-  db.collection('users').insertOne({
-    studentName: req.body.studentName,
-    studentNumber: req.body.studentNumber
-  }, done)
-  function done(err) {
-    if (err) {
-      next(err)
-    } else {
-      res.redirect('/q1WAFS')
-    }
-  }
-}
-// console.log(db.collection('users').find({studentName: "Inju Michorius"}))
-//Insert data to the database
-app.post('/send-q1WAFS', teacher)
-
-function teacher(req, res, next) {
-  db.collection('users').find({
-    studentNumber: '500804843'
-  }, done)
-  function done(err) {
-    if (err) {
-      next(err)
-    } else {
-      res.redirect('/q2WAFS')
-    }
-  }
-}
-
+const app = express()
+const router = express.Router()
+const port = 3000
 
 //Identifying default path
 app.use(express.static(__dirname + '/public/'));
@@ -80,8 +49,67 @@ app.use(express.static(__dirname + '/public/'));
 app.set('views', 'view')
 app.set('view engine', 'ejs')
 
-//Routes
-app.get('/', account)
-app.get('/q1WAFS', q1WAFS)
-app.get('/q2WAFS', q2WAFS)
-app.get('/q3WAFS', q3WAFS)
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+app.get('/', (req, res) => {
+  res.render('account', {
+    uuid: uuidv4().toString()
+  })
+});
+
+app.get('/send-account/:uuid', (req, res) => {
+  try {
+    const student = {
+      uuid: req.params.uuid,
+      studentName: req.query.studentName,
+      studentNumber: req.query.studentNumber
+    };
+
+    const data = new StudentPersistence(student)
+    data.save();
+
+    res.render('./WAFS/q1WAFS', {
+      uuid: req.params.uuid
+    })
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get('/send-q1WAFS/:uuid', (req, res) => {
+  try {
+    const wafsRating = {
+      uuid: req.params.uuid,
+      startDate: req.query.startDate,
+      endDate: req.query.endDate,
+      lessonMarterial: req.query.lessonMarterial,
+      explanation: req.query.explanation,
+      ownInsight: req.query.ownInsight,
+    };
+
+    const data = new WAFSPersistence(wafsRating)
+    data.save();
+
+    res.render('./CTTR/q1CTTR', {
+      uuid: req.params.uuid
+    })
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+// app.get('/send-qCTTR/:uuid', (req, res) => {
+//   try {
+
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
