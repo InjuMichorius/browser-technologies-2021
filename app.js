c436744687d1c28972e31542e19927d11aa62ecf
partychickenking <partychickenking@gmail.com>
2021-03-27 14:34:25 +0100

    Added different computer

--- app.js
+++ app.js
@@ -259,5 +259,5 @@ app.get('/send-HCD/:uuid', (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at: http://localhost:${port}`)
 })
