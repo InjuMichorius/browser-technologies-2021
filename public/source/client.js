26c9c98319653956a0c3ff8081c404a5cfb1222e
partychickenking <partychickenking@gmail.com>
2021-03-30 02:40:03 +0200

    Added js and styles

---
+++ public/source/client.js
@@ -0,0 +1,17 @@
+const images = [
+    '../img/background-image.jpg',
+    '../img/background-image3.jpg',
+    '../img/background-image4.jpg',
+    '../img/background-image5.jpg'
+]
+
+const img = document.querySelector('html')
+
+function imgDisp() {
+    const num = Math.floor(Math.random() * 6)
+    img.style.backgroundImage = 'url("' + images[num] + '")'
+}
+
+imgDisp()
+
+console.log('Hello world!')
